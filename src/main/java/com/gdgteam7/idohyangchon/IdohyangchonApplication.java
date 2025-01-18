728b93dcf43522d84a81ad8b6bb71fe0e05f21ab
Jaehwan Hyun <jec0101@naver.com>
2025-01-18 21:29:01 +0900

    Initial Commit

---
+++ src/main/java/com/gdgteam7/idohyangchon/IdohyangchonApplication.java
@@ -0,0 +1,13 @@
+package com.gdgteam7.idohyangchon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdohyangchonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdohyangchonApplication.class, args);
+	}
+
+}

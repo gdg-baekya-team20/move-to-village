73a002a9d3e5123e4081f9557ae58be7047f1670
JaehwanH <54016683+ja7811@users.noreply.github.com>
2025-01-19 04:29:47 +0900

    [UPDATE] ruralArea 테이블 추가 (#8)
    * [DELETE] gitkeep 삭제
    
    * [ADD] ruralArea 엔티티 추가
    
    * [ADD] ruralArea 엔티티 추가

---
+++ src/main/java/com/gdgteam7/idohyangchon/domain/RuralArea.java
@@ -0,0 +1,32 @@
+package com.gdgteam7.idohyangchon.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RuralArea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String ruralName;
+
+    private Integer housingCost;
+    private Integer foodCost;
+    private Integer transportationCost;
+
+    private Integer transportationScore;
+    private Integer cultureScore;
+    private Integer medicalScore;
+    private Integer greenScore;
+    private Integer costScore;
+
+    private String thumbnailUrl;
+    private String localGovernmentUrl;
+
+}
